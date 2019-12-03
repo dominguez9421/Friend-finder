2e039f87ccaff33de55d0dd6ba1ab3d0dad12746
Jorge Dominguez <jordom300@gmail.com>
2019-12-02 22:22:54 -0600

    added home/surbey/server js

---
+++ friendfinder/app/data/friends.js
@@ -0,0 +1,38 @@
+var friends = [
+
+     // Ex: Array from gitlab instructions 
+    {
+        name: "Ahmed",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [ 5,1,4,4,5,1,2,5,4,1]
+    },
+    {
+        name: "The rock",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRLrdwRrqUORpHhhrf0D6NysDfN12zmmRE7OtQu_-Nv1G43ertXg&s",
+       scores: [4, 5, 5, 3, 5, 5, 5, 2, 5, 1]
+    },
+    {
+        name: "Charlie chapin",
+        photo: "https://thewire.in/wp-content/uploads/2017/07/Charlie-Chaplin-5.jpg",
+       scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+    {
+        name: "Doge",
+        photo: "https://i.kym-cdn.com/entries/icons/original/000/013/564/doge.jpg",
+       scores: [1, 2, 3, 5, 1, 1, 2, 1, 2, 1]
+    }, 
+    {
+
+        name: "Fat Gotenks",
+        photo: "https://i.ytimg.com/vi/OR3bceMSGdA/maxresdefault.jpg", 
+       scores: [1,2,2,1,3,2,4,3,2,1]
+    },
+    {
+        name: "Cat",
+        photo: "https://image.slidesharecdn.com/downloadfunnycatvideos-150906143836-lva1-app6891/95/download-funny-cat-videos-1-638.jpg?cb=1441550352", 
+       scores: [5,4,3,5,4,5,4,5,3,4]
+    },
+
+]; 
+
+module.exports = friends; 
--- server.js
+++ server.js
@@ -1,31 +1,60 @@
-const express = require('express')
-const app = express()
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
+
+
 var bodyParser = require('body-parser')
- 
- 
+var express = require("express")
+
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 8080; 
 
 
-var bodyParser = require('body-parser')
+//test
+//  app.get('/', function (req, res){
+//      res.send('hello world!'); 
+//  })
+ 
+// app.use(express.urlencoded({extended:true})); 
+// app.use(express.json()); 
+
+// app.use(express.static('css'));
+
  
  
-// create application/json parser
-var jsonParser = bodyParser.json()
- 
-// create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+// Sets up the Express app to handle data parsing
+ app.use(bodyParser.urlencoded({ extended: true }));
 
 
  
-// parse various different custom JSON types as JSON
+// // parse various different custom JSON types as JSON
 app.use(bodyParser.json({ type: 'application/*+json' }))
  
-// parse some custom thing into a Buffer
-app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+// // parse some custom thing into a Buffer
+ app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
  
-// parse an HTML body into a string
-app.use(bodyParser.text({ type: 'text/html' }))
+// // parse an HTML body into a string
+ app.use(bodyParser.text({ type: 'text/html' }))
 
+ // ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+
+require("./friendfinder/app/routing/api-routes")(app); 
+
+require("./friendfinder/app/routing/html-routes")(app); 
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
 
 app.listen(PORT, function (){
     console.log("App is listening to PORT: " + PORT); 
